903f60c932203dd6581d849a5107b2c695fab383
Abner B <Abner3@users.noreply.github.com>
2016-11-15 06:00:20 -0800

    Update Drivebase.h

--- Drivebase.h
+++ Drivebase.h
@@ -3,7 +3,7 @@
 void setLeftWheelSpeed ( int speed = 127 );
 void setRightWheelSpeed ( int speed = 127 );
 
-//Sets both sides of hte drivebase to differient speeds
+//Sets both sides of the drivebase to differient speeds
 #warning "JonLib: Drivebase"
 void setWheelSpeed ( int leftWheelSpeed = 127, int rightWheelSpeed = 127 ) {
 	setLeftWheelSpeed(leftWheelSpeed);
